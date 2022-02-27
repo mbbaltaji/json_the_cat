46fb476a50ad1b9bc6500f2e203121995e674927
Mustapha Baltaji <mbbaltaji@gmail.com>
2022-02-27 16:34:12 +0000

    refactor: use callback to handle errors and printing in index.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,28 +1,21 @@
 const request = require('request');
-const arg = process.argv.slice(2);
 
-
-
-request(`https://api.thessscatapi.com/v1/breeds/search?q=${arg}`, (error, response, body) =>{
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) =>{
   
-  if (error) {
-    console.log(error);
-    return;
-  }
-   
-  //console.log('body: ', body);
-   
-  let data = '';
-  if (body) {
-    data = JSON.parse(body);
-    console.log(response.statusCode);
-  }
-  //console.log(data);
-
-  if (data.length > 0) {
-    console.log(data[0].description);
-  } else {
-    console.log(`Invalid breed name ${arg}, Breed not found..`);
-  }
+    if (error) {
+      callback(error, null);
+      return;
+    }
   
-});
+    let data = JSON.parse(body);
+  
+    if (data.length > 0) {
+      callback(null,data[0].description); // on success
+    } else {
+      callback(`Invalid breed name '${breedName}', Breed not found..`, null);
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
